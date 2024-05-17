9b338249a38ec79155171dec68b904bb746391a8
Matthieu Fontaine <matth76330@live.fr>
2024-05-17 15:13:00 +0200

    add i18n

--- password-checker/src/App.tsx
+++ password-checker/src/App.tsx
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { useTranslation } from "react-i18next";
-import './i18n';
 
 export default function App() {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
   const [theme, setTheme] = useState('light');
 
   const [password, setPassword] = useState<string>("");
@@ -18,15 +17,14 @@ export default function App() {
     } else {
       setTheme('light');
     }
-    console.log(theme);
   };
 
   const toggleLang = () => {
-    //if (i18n.language === 'fr') {
-    //  i18n.changeLanguage('en');
-    //} else {
-    //  i18n.changeLanguage('fr');
-    //}
+    if (i18n.language === 'fr') {
+      i18n.changeLanguage('en');
+    } else {
+      i18n.changeLanguage('fr');
+    }
   }
 
   useEffect(() => {
@@ -66,36 +64,36 @@ export default function App() {
         <header className="bg-gray-800 text-white text-center p-4 font-bold text-2xl">
           Password Checker
         </header>
-        <div className="w-full justify-center items-center flex flex-col">
-          <label htmlFor="theme" className="inline-flex items-center space-x-4 cursor-pointer dark:text-gray-100 mt-2">
-            <span className="text-black">Dark</span>
+        <div className="w-full flex items-center justify-between ">
+          <label htmlFor="theme" className=" ml-4 inline-flex items-center space-x-4 cursor-pointer dark:text-gray-100 mt-2">
+            <span className="text-black">{t('clair')}</span>
             <span className="relative">
               <input id="theme" type="checkbox" className="hidden peer" onClick={toggleTheme} />
               <div className="w-10 h-6 rounded-full shadow-inner dark:bg-gray-400 peer-checked:dark:bg-violet-400"></div>
               <div className="absolute inset-y-0 left-0 w-4 h-4 m-1 rounded-full shadow peer-checked:right-0 peer-checked:left-auto dark:bg-gray-800"></div>
             </span>
-            <span className="text-black">Light</span>
+            <span className="text-black">{t('sombre') }</span>
           </label>
 
-          <label htmlFor="langue" className="inline-flex items-center space-x-4 cursor-pointer dark:text-gray-100 mt-2">
-            <span className="text-black">français</span>
+          <label htmlFor="langue" className="mr-4 inline-flex items-center space-x-4 cursor-pointer dark:text-gray-100 mt-2">
+            <span className="text-black">{t('francais')}</span>
             <span className="relative">
               <input id="langue" type="checkbox" className="hidden peer" onClick={toggleLang} />
               <div className="w-10 h-6 rounded-full shadow-inner dark:bg-gray-400 peer-checked:dark:bg-violet-400"></div>
               <div className="absolute inset-y-0 left-0 w-4 h-4 m-1 rounded-full shadow peer-checked:right-0 peer-checked:left-auto dark:bg-gray-800"></div>
             </span>
-            <span className="text-black">Anglais</span>
+            <span className="text-black">{t('anglais')}</span>
           </label>
+        </div>
 
-          <div className="w-full justify-center items-center flex flex-col">
-            <input type="password" className="border border-gray-300 rounded-lg p-2 m-2 w-5/6 lg:w-1/3" placeholder={t('tapermotdepasse')} value={password} onChange={handleChange} />
-            <div className="flex flex-col lg:flex-row gap-3">
-              <span className="text-gray-800 font-bold">{password.length} {t('contient')} : </span>
-              <span className={`text-gray-500 text-base lg:text-sm font-bold ${passwordLower ? 'text-red-400' : ''}`}>{t('minuscule')}</span>
-              <span className={`text-gray-500 text-base lg:text-sm font-bold ${passwordUpper ? 'text-red-400' : ''}`}>{t('majuscule')}</span>
-              <span className={`text-gray-500 text-base lg:text-sm font-bold ${passwordNumbers ? 'text-red-400' : ''}`}>{t('nombre')}</span>
-              <span className={`text-gray-500 text-base lg:text-sm font-bold ${passwordSpecial ? 'text-red-400' : ''}`}>{t('characterespeciaux')}</span>
-            </div>
+        <div className="w-full h-full justify-center items-center flex flex-col">
+          <input type="password" className="border border-gray-300 rounded-lg p-2 m-2 w-5/6 lg:w-1/3" placeholder={t('tapermotdepasse')} value={password} onChange={handleChange} />
+          <div className="flex flex-col lg:flex-row gap-3">
+            <span className="text-gray-800 font-bold">{password.length} {t('contient')} : </span>
+            <span className={`text-gray-500 text-base lg:text-sm font-bold ${passwordLower ? 'text-red-400' : ''}`}>{t('minuscule')}</span>
+            <span className={`text-gray-500 text-base lg:text-sm font-bold ${passwordUpper ? 'text-red-400' : ''}`}>{t('majuscule')}</span>
+            <span className={`text-gray-500 text-base lg:text-sm font-bold ${passwordNumbers ? 'text-red-400' : ''}`}>{t('nombre')}</span>
+            <span className={`text-gray-500 text-base lg:text-sm font-bold ${passwordSpecial ? 'text-red-400' : ''}`}>{t('characterespeciaux')}</span>
           </div>
 
         </div>
